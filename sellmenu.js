7fa4c18c06b2f6d4a029272bbeda7be9a1285b2e
mikejunt <mikejunt@gmail.com>
2020-02-04 10:02:57 -0600

    cleans up the console logs

--- sellmenu.js
+++ sellmenu.js
@@ -1,23 +1,15 @@
 var salemenu = document.getElementById("salemenu");
-var relevant = 576;
 window.addEventListener("resize", function () {
     resize();
 });
 function resize() {
-    console.log("" + window.innerWidth);
     if (window.innerWidth < 576) {
-        console.log(window.innerWidth);
         salemenu.remove();
         document.getElementById("saleitem").style.width = "100%";
-        console.log("try to remove fired");
     }
-    console.log(window.innerWidth > relevant);
     if (window.innerWidth > 575) {
         var replace = document.getElementById("saleparent");
-        console.log("Hello");
         replace.prepend(salemenu);
-        console.log("tried to prepend the menu");
         document.getElementById("saleitem").style.width = "calc(100% - 111px)";
-        console.log("tried to change saleitem style");
     }
 }
--- sellmenu.ts
+++ sellmenu.ts
@@ -1,29 +1,17 @@
 const salemenu = document.getElementById("salemenu");
-const relevant = 576;
 
 window.addEventListener("resize", function () {
     resize();
 })
 
-
-
-
-
 function resize() :void {
-    console.log(`${window.innerWidth}`);
     if (window.innerWidth < 576) {
-        console.log(window.innerWidth)
         salemenu.remove();
         document.getElementById("saleitem").style.width = "100%"
-        console.log("try to remove fired")
     }
-    console.log(window.innerWidth > relevant);
     if (window.innerWidth > 575) {
         let replace = document.getElementById("saleparent");
-        console.log("Hello");
         replace.prepend(salemenu);
-        console.log("tried to prepend the menu")
         document.getElementById("saleitem").style.width = "calc(100% - 111px)"
-        console.log("tried to change saleitem style")
     }
 }
